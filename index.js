1b13bd431b5071e2016da6476fd474d1b85e2c29
erik.fuko <erik.fuko@szitar.net>
2023-03-02 13:34:21 +0100

    -

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const  PORT = 3000;
 app.use(cors());
 app.use(express.json())
 
-
 app.get("/api/get", (req,res)=>{
     db.query("SELECT * FROM adatok", (err,result)=>{
         if(err) {
@@ -16,6 +15,8 @@ app.get("/api/get", (req,res)=>{
     res.send(result)
     });   });
 
+
+    //Január    
 app.get("/api/get/jan/bev", (req,res)=>{
     db.query("SELECT SUM(bevetel) FROM adatok WHERE honap = 'Január'", (err,result)=>{
         if(err) {
@@ -32,6 +33,7 @@ app.get("/api/get/jan/kiad", (req,res)=>{
     res.send(result)
     });   });
 
+    //Február
 app.get("/api/get/feb/bev", (req,res)=>{
     db.query("SELECT SUM(bevetel) FROM adatok WHERE honap = 'Február'", (err,result)=>{
         if(err) {
@@ -47,5 +49,39 @@ app.get("/api/get/feb/kiad", (req,res)=>{
         } 
     res.send(result)
     });   });
+
+    //Március
+    app.get("/api/get/mar/bev", (req,res)=>{
+        db.query("SELECT SUM(bevetel) FROM adatok WHERE honap = 'Március'", (err,result)=>{
+            if(err) {
+            console.log(err)
+            } 
+        res.send(result)
+        });   });
+    
+    app.get("/api/get/mar/kiad", (req,res)=>{
+        db.query("SELECT SUM(kiadas) FROM adatok WHERE honap = 'Március'", (err,result)=>{
+            if(err) {
+            console.log(err)
+            } 
+        res.send(result)
+        });   });
+
+    //Április
+    app.get("/api/get/apr/bev", (req,res)=>{
+        db.query("SELECT SUM(bevetel) FROM adatok WHERE honap = 'Április'", (err,result)=>{
+            if(err) {
+            console.log(err)
+            } 
+        res.send(result)
+        });   });
+    
+    app.get("/api/get/apr/kiad", (req,res)=>{
+        db.query("SELECT SUM(kiadas) FROM adatok WHERE honap = 'Április'", (err,result)=>{
+            if(err) {
+            console.log(err)
+            } 
+        res.send(result)
+        });   });
 
 app.listen(process.env.PORT || 3000)
